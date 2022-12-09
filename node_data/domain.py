be74381497079959679e449b56faafcbc02f3cb6
bnymnsen <106750021+bnymnsen@users.noreply.github.com>
2022-12-09 17:43:39 +0300

    Update domain.py

--- node_data/domain.py
+++ node_data/domain.py
@@ -5,13 +5,16 @@ from contextlib import ExitStack
 from bioregistry import normalize_curie
 from tqdm import tqdm
 
+from time import time
+from biocypher._logger import logger
+
 class InterPro:
     """
     Class that downloads InterPro data using pypath and reformats it to be ready
     for import into a BioCypher database.
     """
 
-    def __init__(self, cache=False, debug=False, page_size=150, retries=6):
+    def __init__(self, cache=False, debug=False, page_size=150, retries=6, organism=None):
         """        
         Args:
             cache: if True, it uses the cached version of the data, otherwise
@@ -19,18 +22,21 @@ class InterPro:
             debug: if True, turns on debug mode in pypath.
             retries: number of retries in case of download error.
             page_size: page size of downloaded annotation data
+            organism: taxonomy id of selected organism, if None take all available organisms
         """
         self.cache = cache
         self.debug = debug
         self.retries = retries
         self.page_size = page_size
+        self.organism = organism
+        
 
 
     def download_domain_node_data(self):
         """
         Downloads domain node data from Interpro
         """
-
+        
         # stack pypath context managers
         with ExitStack() as stack:
 
@@ -41,12 +47,19 @@ class InterPro:
 
             if not self.cache:
                 stack.enter_context(curl.cache_off())
-                
+            
+            logger.debug("Started downloading InterPro domain data")
+            t0 = time()
+            
+            # To do: Filter according to tax id??
             self.interpro_entries = interpro.interpro_entries() # returns a list of namedtuples
             self.interpro_structural_xrefs = interpro.interpro_xrefs(db_type = 'structural')
             self.interpro_external_xrefs = interpro.interpro_xrefs(db_type = 'external')
             
-    def download_domain_edge_data(self, cache=False, debug=False, retries=3):
+            t1 = time()
+            logger.info(f'InterPro domain data is downloaded in {round((t1-t0) / 60, 2)} mins')
+            
+    def download_domain_edge_data(self):
         """
         Downloads Uniprot annotation data from Interpro
         """
@@ -61,8 +74,17 @@ class InterPro:
             if not self.cache:
                 stack.enter_context(curl.cache_off())
             
-            # WARNING: decrease page_size parameter if there is a curl error about timeout in the pypath_log
-            self.interpro_annotations = interpro.interpro_annotations(page_size = self.page_size, reviewed = True, tax_id = '')       
+            logger.debug("Started downloading InterPro annotation data")
+            t0 = time()
+            
+            if self.organism:                
+                # WARNING: decrease page_size parameter if there is a curl error about timeout in the pypath_log
+                self.interpro_annotations = interpro.interpro_annotations(page_size = self.page_size, reviewed = True, tax_id = self.organism)
+            else:
+                self.interpro_annotations = interpro.interpro_annotations(page_size = self.page_size, reviewed = True, tax_id = '')
+            
+            t1 = time()
+            logger.info(f'InterPro annotation data is downloaded in {round((t1-t0) / 60, 2)} mins')
 
     def get_interpro_nodes(self):
         """
@@ -75,6 +97,9 @@ class InterPro:
         # selected attributes
         primary_attributes = ["protein_count", "name", "type", "parent_list", "child_list"]
         external_attributes = ["pfam", "ec", "pdb"]
+        
+        logger.debug("Creating domain nodes")
+        t0 = time()
 
         for entry in tqdm(self.interpro_entries):
             props = dict()
@@ -111,11 +136,17 @@ class InterPro:
                         else:
                             props[element] = self.interpro_structural_xrefs.get(entry.interpro_id).get(str(element).upper())
                 
-            self.node_list.append((domain_id, "domain", props))
-
+            self.node_list.append((domain_id, "domain", props))        
+        
+        t1 = time()
+        logger.info(f'InterPro nodes created in {round((t1-t0) / 60, 2)} mins')
+    
     def get_interpro_edges(self):
         # create list of edges
         self.edge_list = []
+        
+        logger.debug("Creating protein-domain edges")
+        t0 = time()
 
         # DOMAIN-PROTEIN EDGES
         for k, v in tqdm(interpro_annotations.items()):            
@@ -124,3 +155,6 @@ class InterPro:
                 interpro_id = normalize_curie("interpro:" + annotation.interpro_id)
                 uniprot_id = normalize_curie("uniprot:" + k)                
                 self.edge_list.append((None, uniprot_id, interpro_id, "Has", {"locations":list(annotation.locations)}))
+                
+        t1 = time()
+        logger.info(f'InterPro edges created in {round((t1-t0) / 60, 2)} mins')
