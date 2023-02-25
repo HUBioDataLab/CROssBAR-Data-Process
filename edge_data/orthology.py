25d5486132ba4ddb19cfc272134326ef12f43a3a
bnymnsen <106750021+bnymnsen@users.noreply.github.com>
2023-02-25 21:09:43 +0300

    Update orthology.py

--- edge_data/orthology.py
+++ edge_data/orthology.py
@@ -12,24 +12,28 @@ from tqdm import tqdm
 
 
 OMA_ORGANISMS = {
-    9606,
-    10090,
+    4932, # s. cerevisiae
+    10090, # mouse
     3702,
-    10116,
+    10116, # rat
     559292,
-    9913,
+    9913, # cow
     1264690,
     83333,
-    6239,
+    6239, # c. elegans
     1423,
     39947,
     44689,
-    7227,
-    8355,
-    7955,
-    9031,
+    7227, # drosophila
+    8355, # Xenopus laevis
+    7955, # zebrafish
+    9031, # chicken
     1773,
     9601,
+    9598, # chimp - APES TOGETHER STRONG
+    9544, # Macaca - APES TOGETHER STRONG
+    9595, # GORILLA GORILLA GORILLA - APES TOGETHER STRONG
+    9601, # orangutan - APES TOGETHER STRONG
 }.union(set(taxonomy.taxids.keys()))
 
 
@@ -38,10 +42,6 @@ class Orthology:
     Class that downloads orthology data using pypath and reformats it to be ready
     for import into a BioCypher database.
     """
-
-    def __init__(self):
-        pass
-
     
     def download_orthology_data(self, cache=False, debug=False, retries=3,):
         """
@@ -66,11 +66,16 @@ class Orthology:
 
             self.download_oma_data()
             self.download_pharos_data()
-    
+            
+            
+    def process_orthology_data(self):
+        
+        self.process_oma_data()
+        self.process_pharos_data()
 
     def download_oma_data(self, tax=OMA_ORGANISMS):
         """
-        Downloads orthology data from OMA.
+        Downloads orthology data from OMA against human.
 
         Args
             tax: list of taxids to download data for.
@@ -92,7 +97,30 @@ class Orthology:
         t1 = time()
         logger.info(f'OMA orthology data is downloaded in {round((t1-t0) / 60, 2)} mins')
 
-    
+    def process_oma_data(self):
+        """
+        Processes orthology data from OMA.
+        """
+        
+        logger.debug("Started processing OMA orthology data")
+        t0 = time()
+        
+        df_list = []
+        for ortholog in self.oma_orthology:
+            df_list.append((self.entry_name_to_uniprot[ortholog.id_a], self.entry_name_to_uniprot[ortholog.id_b], 
+                           ortholog.rel_type, round(ortholog.score)))
+
+        oma_orthology_df = pd.DataFrame(df_list, columns=["uniprot_a", "uniprot_b", "relation_type", "oma_orthology_score"])
+
+        oma_orthology_df["source"] = "OMA"
+        
+        oma_orthology_df.sort_values(by="oma_orthology_score", ascending=False, inplace=True)
+        
+        self.oma_orthology_duplicate_removed_df = oma_orthology_df[~oma_orthology_df[["uniprot_a", "uniprot_b"]].apply(frozenset, axis=1).duplicated()].reset_index(drop=True)
+        
+        t1 = time()
+        logger.info(f'OMA orthology data is processed in {round((t1-t0) / 60, 2)} mins')
+        
     def download_pharos_data(self):
         """
         Downloads orthology data from Pharos.
@@ -105,51 +133,104 @@ class Orthology:
         self.entrez_to_uniprot = {}
         for k,v in uniprot_to_entrez.items():
             for entrez in v.split(';'):
-                if entrez != '':
+                if entrez:
                     self.entrez_to_uniprot[entrez] = k
    
-        pharos_orthology_init = pharos.pharos_targets(orthologs=True)
-        self.pharos_orthology = []
-        for protein in pharos_orthology_init:
-            if protein['orthologs']:
-                for ortholog in protein['orthologs']:
-                    if str(ortholog['geneid']) in self.entrez_to_uniprot:
-                        self.pharos_orthology.append(
-                            {
-                                'id_a': protein['uniprot'],
-                                'id_b': self.entrez_to_uniprot[str(ortholog['geneid'])],
-                                'db_id_b' : ortholog['dbid'],
-                            })
-
+        self.pharos_orthology_init = pharos.pharos_targets(orthologs=True)
+    
         t1 = time()
         logger.info(f'Pharos orthology data is downloaded in {round((t1-t0) / 60, 2)} mins')
-
     
-    def get_orthology_edges(self):
+    def process_pharos_data(self):
+        """
+        Processes orthology data from Pharos.
+        """
+        logger.debug("Started processing Pharos orthology data")
+        t0 = time()
+        
+        selected_species = ['Mouse', 'Cow', 'Xenopus', 'Zebrafish', 'Rat', 'C. elegans', 'S.cerevisiae', 'Chicken', 'Chimp', 'Fruitfly', 'Dog', 'Macaque', 'Pig', 'Horse',]
+
+        df_list = []
+        for protein in self.pharos_orthology_init:
+            if protein["orthologs"]:
+                for ortholog in protein["orthologs"]:
+                    if ortholog['geneid'] and str(ortholog['geneid']) in self.entrez_to_uniprot and str(ortholog['species']) in selected_species:
+                        df_list.append((protein["uniprot"], self.entrez_to_uniprot[str(ortholog['geneid'])]))
+                
+        
+        pharos_orthology_df = pd.DataFrame(df_list, columns=["uniprot_a", "uniprot_b"])
+
+        pharos_orthology_df["source"] = "Pharos"
+        
+        self.pharos_orthology_duplicate_removed_df = pharos_orthology_df[~pharos_orthology_df[["uniprot_a", "uniprot_b"]].apply(frozenset, axis=1).duplicated()].reset_index(drop=True)
+
+        t1 = time()
+        logger.info(f'Pharos orthology data is processed in {round((t1-t0) / 60, 2)} mins')
+
+    def merge_source_column(self, element, joiner="|"):
+        """
+        Merges source columns' entries in selected dataframes
+        """
+        _list = []
+        for e in list(element.dropna().values):
+            if joiner in e:
+                for i in e.split(joiner):
+                    _list.append(i)
+            else:
+                _list.append(e)            
+
+        return joiner.join(list(dict.fromkeys(_list).keys()))
+    
+    def merge_orthology_data(self):
+        """
+        Merges orthology data from OMA and Pharos
+        """
+        logger.debug("Started merged OMA and Pharos orthology data")
+        t0 = time()
+        
+        self.oma_orthology_duplicate_removed_df
+        
+        self.pharos_orthology_duplicate_removed_df
+        
+        oma_plus_pharos_orthology_df = self.oma_orthology_duplicate_removed_df.merge(self.pharos_orthology_duplicate_removed_df, how="outer",
+                                                                       on=["uniprot_a", "uniprot_b"])
+        
+        oma_plus_pharos_orthology_df["source"] = oma_plus_pharos_orthology_df[["source_x", "source_y"]].apply(self.merge_source_column, axis=1)
+        
+        oma_plus_pharos_orthology_df.drop(columns=["source_x", "source_y"], inplace=True)
+        
+        self.all_orthology_df = oma_plus_pharos_orthology_df
+        
+        t1 = time()
+        logger.info(f'OMA and Pharos orthology data is merged in {round((t1-t0) / 60, 2)} mins')        
+    
+    def get_orthology_edges(self, early_stopping=500):
         """
         Reformats orthology data to be ready for import into a BioCypher database.
         """
 
         self.edge_list = []
+        
+        logger.info("Preparing orthology edges.")
+        
+        for index, row in tqdm(self.all_orthology_df.iterrows(), total=self.all_orthology_df.shape[0]):
+            _dict = row.to_dict()
+            
+            source = normalize_curie('uniprot:' + _dict["uniprot_a"])
+            target = normalize_curie('uniprot:' + _dict["uniprot_b"])
+            
+            del _dict["uniprot_a"], _dict["uniprot_b"]
+            
+            props = dict()
+            for k, v in _dict.items():
+                if str(v) != "nan":
+                    if isinstance(v, str) and "|" in v:
+                        props[str(k).replace(" ","_").lower()] = v.split("|")
+                    else:
+                        props[str(k).replace(" ","_").lower()] = v
 
-        self.oma_orthology_edges = []
-        logger.info("Preparing OMA orthology edges.")
-        for orthology in self.oma_orthology:
-            id_a = normalize_curie("uniprot:" + self.entry_name_to_uniprot[orthology.id_a])
-            id_b = normalize_curie("uniprot:" + self.entry_name_to_uniprot[orthology.id_b])
-            props = {
-                'rel_type': orthology.rel_type,
-                'score': orthology.score,
-            }
 
-            self.oma_orthology_edges.append((None, id_a, id_b, 'Is_Ortholog_To', props))
-            self.edge_list.append((None, id_a, id_b, 'Is_Ortholog_To', props))
-
-        self.pharos_orthology_edges = []
-        logger.info("Preparing Pharos orthology edges.")
-        for orthology in self.pharos_orthology:
-            id_a = normalize_curie("uniprot:" + orthology['id_a'])
-            id_b = normalize_curie("uniprot:" + orthology['id_b'])
-
-            self.pharos_orthology_edges.append((None, id_a, id_b, 'Is_Ortholog_To', {'db_id_b': orthology['db_id_b']}))
-            self.edge_list.append((None, id_a, id_b, 'Is_Ortholog_To', {'db_id_b': orthology['db_id_b']}))
+            self.edge_list.append((None, source, target, "protein_is_orthologous_with_protein", props))
+            
+            if early_stopping and (index+1) == early_stopping:
+                break
