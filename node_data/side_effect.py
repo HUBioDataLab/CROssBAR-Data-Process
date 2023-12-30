bdade5107602834fc2177b4c151ec18b486f2274
bnymnsen <106750021+bnymnsen@users.noreply.github.com>
2023-12-30 14:09:59 +0300

    preliminary refactor

--- node_data/side_effect.py
+++ node_data/side_effect.py
@@ -6,27 +6,61 @@ from pypath.inputs import sider, drugbank, offsides, adrecs
 from contextlib import ExitStack
 
 from bioregistry import normalize_curie
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from time import time
+
+from enum import Enum, auto
+from typing import Union
 
 import pandas as pd
 import numpy as np
 
+from biocypher._logger import logger
+
+logger.debug(f"Loading module {__name__}.")
+
+
+class SideEffectNodeField(Enum):
+    NAME = "name"
+    SYNONYMS = "synonyms"
+
+class DrugSideEffectEdgeField(Enum):
+    FREQUENCY = "frequency"
+    PROPORTIONAL_REPORTING_RATIO = "proportional_reporting_ratio"
+
+class SideEffectEdgeType(Enum):
+    DRUG_TO_SIDE_EFFECT = auto()
+    SIDE_EFFECT_HIERARCHICAL_ASSOCIATION = auto()
+
+
 
 class SideEffect:
-    def __init__(self, drugbank_user, drugbank_passwd, add_prefix=True):
+    def __init__(self, drugbank_user, 
+                 drugbank_passwd,
+                 side_effect_node_fields: Union[list[SideEffectNodeField], None] = None,
+                 drug_side_effect_edge_fields: Union[list[DrugSideEffectEdgeField], None] = None,
+                 edge_types: Union[list[SideEffectEdgeType], None] = None,
+                 add_prefix=True):
         self.drugbank_user = drugbank_user
         self.drugbank_passwd = drugbank_passwd
         
         self.add_prefix = add_prefix
-        
+
+        # set node fields
+        self.set_node_fields(side_effect_node_fields=side_effect_node_fields)
+
+        # set edge fields
+        self.set_edge_fields(drug_side_effect_edge_fields=drug_side_effect_edge_fields)
+
+        # set edge types
+        self.set_edge_types(edge_types=edge_types) 
         
     def download_side_effect_data(
         self,
-        cache=False,
-        debug=False,
-        retries=3,
-    ):
+        cache: bool = False,
+        debug: bool = False,
+        retries: int = 3,
+    ) -> None:
         """
         Wrapper function to download side effect data from various databases using pypath.
         Args
@@ -52,64 +86,69 @@ class SideEffect:
             self.download_adrecs_data()
                 
     
-    def dowload_sider_data(self):
+    def dowload_sider_data(self) -> None:
         
-        print("Started downloading Sider data")
+        logger.debug("Started downloading Sider data")
         t0 = time()
-        
-        self.drugbank_data = drugbank.DrugbankFull(user = self.drugbank_user, passwd = self.drugbank_passwd)
-        drugbank_drug_names = self.drugbank_data.drugbank_drugs_full(fields = ["name"])
-        self.drugbank_name_to_drugbank_id_dict = {i.name.lower():i.drugbank_id for i in drugbank_drug_names}
-        
-        sider_drug = sider.sider_drug_names()
-        self.cid_to_sider_drug_name = {k:list(v)[0] for k,v in sider_drug.items()}
-        
+
         sider_meddra_tsv = sider.sider_meddra_side_effects()
-        self.umls_to_meddra_id = {i.cid:{"meddra_id":i.meddra_id, "name":i.side_effect_name} for i in sider_meddra_tsv}
         self.meddra_id_to_side_effect_name = {i.meddra_id:i.side_effect_name for i in sider_meddra_tsv}
         
-        self.sider_meddra_with_freq = sider.sider_side_effect_frequencies()
+        if SideEffectEdgeType.DRUG_TO_SIDE_EFFECT in self.edge_types:
+            self.umls_to_meddra_id = {i.cid:{"meddra_id":i.meddra_id, "name":i.side_effect_name} for i in sider_meddra_tsv}
+
+            self.drugbank_data = drugbank.DrugbankFull(user = self.drugbank_user, passwd = self.drugbank_passwd)
+            drugbank_drug_names = self.drugbank_data.drugbank_drugs_full(fields = ["name"])
+            self.drugbank_name_to_drugbank_id_dict = {i.name.lower():i.drugbank_id for i in drugbank_drug_names}
+            
+            sider_drug = sider.sider_drug_names()
+            self.cid_to_sider_drug_name = {k:list(v)[0] for k,v in sider_drug.items()}     
+        
+            self.sider_meddra_with_freq = sider.sider_side_effect_frequencies()
         
         t1 = time()
-        print(f"Sider data is downloaded in {round((t1-t0) / 60, 2)} mins")
+        logger.info(f"Sider data is downloaded in {round((t1-t0) / 60, 2)} mins")
         
-    def download_offsides_data(self):
-        print("Started downloading OffSides data")
+    def download_offsides_data(self) -> None:
+        logger.debug("Started downloading OffSides data")
         t0 = time()
         
-        if not hasattr(self, "drugbank_data"):
-            self.drugbank_data = drugbank.DrugbankFull(user = self.drugbank_user, passwd = self.drugbank_passwd)
-            
-        drugbank_external_ids = self.drugbank_data.drugbank_external_ids_full()
-        self.rxcui_to_drugbank = {v.get("RxCUI"):k for k, v in drugbank_external_ids.items() if v.get("RxCUI")}
+        if SideEffectEdgeType.DRUG_TO_SIDE_EFFECT in self.edge_types:
+            if not hasattr(self, "drugbank_data"):
+                self.drugbank_data = drugbank.DrugbankFull(user = self.drugbank_user, passwd = self.drugbank_passwd)
+                
+            drugbank_external_ids = self.drugbank_data.drugbank_external_ids_full()
+            self.rxcui_to_drugbank = {v.get("RxCUI"):k for k, v in drugbank_external_ids.items() if v.get("RxCUI")}
         
         self.offsides_data = offsides.offsides_side_effects()
         
         t1 = time()
-        print(f"OffSides data is downloaded in {round((t1-t0) / 60, 2)} mins")
+        logger.info(f"OffSides data is downloaded in {round((t1-t0) / 60, 2)} mins")
         
-    def download_adrecs_data(self):
-        print("Started downloading ADReCS data")
+    def download_adrecs_data(self) -> None:
+        logger.debug("Started downloading ADReCS data")
         t0 = time()
         
-        adrecs_drug_information = adrecs.adrecs_drug_identifiers()
-        self.adrecs_drug_id_to_drugbank_id = {dr.badd: dr.drugbank for dr in adrecs_drug_information if dr.drugbank}
-        
         self.adrecs_terminology = adrecs.adrecs_adr_ontology()
+
+        if SideEffectEdgeType.DRUG_TO_SIDE_EFFECT in self.edge_types:
+            adrecs_drug_information = adrecs.adrecs_drug_identifiers()
+            self.adrecs_drug_id_to_drugbank_id = {dr.badd: dr.drugbank for dr in adrecs_drug_information if dr.drugbank}
+            
+            self.adrecs_adr_id_to_adrecs_drug_id = {dr.adr_badd: dr.drug_badd for dr in adrecs.adrecs_drug_adr()}
+            self.adrecs_adr_id_to_meddra_id = {entry.badd:str(entry.meddra) for entry in self.adrecs_terminology}
         
-        self.adrecs_adr_id_to_adrecs_drug_id = {dr.adr_badd: dr.drug_badd for dr in adrecs.adrecs_drug_adr()}
-        self.adrecs_adr_id_to_meddra_id = {entry.badd:str(entry.meddra) for entry in self.adrecs_terminology}
-        
-        self.adrecs_ontology = adrecs.adrecs_hierarchy()
+        if SideEffectEdgeType.SIDE_EFFECT_HIERARCHICAL_ASSOCIATION in self.edge_types:
+            self.adrecs_ontology = adrecs.adrecs_hierarchy()
         
         t1 = time()
-        print(f"ADReCS data is downloaded in {round((t1-t0) / 60, 2)} mins")
+        logger.info(f"ADReCS data is downloaded in {round((t1-t0) / 60, 2)} mins")
         
-    def process_sider_drug_side_effect(self):
+    def process_sider_drug_side_effect(self) -> pd.DataFrame:
         if not hasattr(self, "sider_meddra_with_freq"):
             self.dowload_sider_data()
             
-        print("Started processing Sider drug-side effect data")
+        logger.debug("Started processing Sider drug-side effect data")
         t0 = time()
         
         df_list = []
@@ -129,15 +168,15 @@ class SideEffect:
         df["source"] = "Sider"
         
         t1 = time()
-        print(f"Sider drug-side effect data is processed in {round((t1-t0) / 60, 2)} mins")
+        logger.info(f"Sider drug-side effect data is processed in {round((t1-t0) / 60, 2)} mins")
         
         return df
     
-    def process_offsides_drug_side_effect(self):
+    def process_offsides_drug_side_effect(self) -> pd.DataFrame:
         if not hasattr(self, "offsides_data"):
             self.download_offsides_data()
             
-        print("Started processing OffSides drug-side effect data")
+        logger.debug("Started processing OffSides drug-side effect data")
         t0 = time()
         
         df_list = []
@@ -153,15 +192,15 @@ class SideEffect:
         df["source"] = "OffSides"
         
         t1 = time()
-        print(f"OffSides drug-side effect data is processed in {round((t1-t0) / 60, 2)} mins")
+        logger.info(f"OffSides drug-side effect data is processed in {round((t1-t0) / 60, 2)} mins")
         
         return df
     
-    def process_adrecs_drug_side_effect(self):
+    def process_adrecs_drug_side_effect(self) -> pd.DataFrame:
         if not hasattr(self, "adrecs_terminology"):
             self.download_adrecs_data()
             
-        print("Started processing ADReCS drug-side effect data")
+        logger.debug("Started processing ADReCS drug-side effect data")
         t0 = time()
         
         df_list = []
@@ -177,18 +216,18 @@ class SideEffect:
         df["source"] = "ADReCS"           
         
         t1 = time()
-        print(f"ADReCS drug-side effect data is processed in {round((t1-t0) / 60, 2)} mins")
+        logger.info(f"ADReCS drug-side effect data is processed in {round((t1-t0) / 60, 2)} mins")
         
         return df
     
-    def merge_drug_side_effect_data(self):
+    def merge_drug_side_effect_data(self) -> pd.DataFrame:
         adrecs_df = self.process_adrecs_drug_side_effect()
         
         sider_df = self.process_sider_drug_side_effect()
         
         offsides_df = self.process_offsides_drug_side_effect()
         
-        print("Started merging drug-side effect edge data")
+        logger.debug("Started merging drug-side effect edge data")
         t0 = time()
         
         merged_df = pd.merge(adrecs_df, sider_df, how="outer", on=["drugbank_id", "meddra_id"])
@@ -204,11 +243,11 @@ class SideEffect:
         merged_df.drop(columns=["source_x", "source_y"], inplace=True)
         
         t1 = time()
-        print(f"Drug-side effect edge data is merged in {round((t1-t0) / 60, 2)} mins")
+        logger.info(f"Drug-side effect edge data is merged in {round((t1-t0) / 60, 2)} mins")
         
         return merged_df
     
-    def get_nodes(self, label="side_effect"):
+    def get_nodes(self, label: str = "side_effect") -> list[tuple]:
         if not hasattr(self, "meddra_id_to_side_effect_name"):
             sider_meddra_tsv = sider.sider_meddra_side_effects()
             self.meddra_id_to_side_effect_name = {i.meddra_id:i.side_effect_name for i in sider_meddra_tsv}
@@ -232,13 +271,16 @@ class SideEffect:
             if entry.synonyms:
                 adr_synonyms_dict[str(entry.meddra)] = list(entry.synonyms)[0].replace("|", ",").replace("'","^") if len(entry.synonyms) == 1 else [t.replace("|", ",").replace("'","^") for t in entry.synonyms]
                 
-        print("Started writing side effect nodes")
+        logger.info("Started writing side effect nodes")
         
         node_list = []
         for meddra_term, condition_name in tqdm(self.meddra_id_to_side_effect_name.items()):
-            props = {"name":condition_name.replace("|", ",").replace('"',"").replace("'","^")}
-            
-            if adr_synonyms_dict.get(meddra_term):
+            props = {}
+
+            if "name" in self.side_effect_node_fields:
+                props["name"] = condition_name.replace("|", ",").replace('"',"").replace("'","^")
+                        
+            if "synonyms" in self.side_effect_node_fields and adr_synonyms_dict.get(meddra_term):
                 props["synonyms"] = adr_synonyms_dict[meddra_term]
                 
             meddra_id = self.add_prefix_to_id(prefix="meddra", identifier=meddra_term)
@@ -246,22 +288,24 @@ class SideEffect:
             
         return node_list
     
-    def get_edges(self):
+    def get_edges(self) -> list[tuple]:
         
-        print("Started writing ALL edge types")
+        logger.info("Started writing ALL edge types")
         
         edge_list = []
         
-        edge_list.extend(self.get_drug_side_effect_edges())
+        if SideEffectEdgeType.SIDE_EFFECT_HIERARCHICAL_ASSOCIATION in self.edge_types:
+            edge_list.extend(self.get_drug_side_effect_edges())
         
-        edge_list.extend(self.get_adrecs_side_effect_hierarchical_edges())
+        if SideEffectEdgeType.DRUG_TO_SIDE_EFFECT in self.edge_types:
+            edge_list.extend(self.get_adrecs_side_effect_hierarchical_edges())
         
         return edge_list
             
-    def get_drug_side_effect_edges(self, label="drug_has_side_effect"):
+    def get_drug_side_effect_edges(self, label: str = "drug_has_side_effect") -> list[tuple]:
         drug_side_effect_edges_df = self.merge_drug_side_effect_data()
         
-        print("Started writing drug-side effect edges")
+        logger.info("Started writing drug-side effect edges")
         
         edge_list = []
         for index, row in tqdm(drug_side_effect_edges_df.iterrows(), total=drug_side_effect_edges_df.shape[0]):
@@ -274,7 +318,7 @@ class SideEffect:
             
             props = {}
             for k, v in _dict.items():
-                if str(v) != "nan":
+                if k in self.drug_side_effect_edge_fields and str(v) != "nan":
                     if isinstance(v, str) and "|" in v:
                         props[k] = v.split("|")
                     else:
@@ -284,11 +328,11 @@ class SideEffect:
             
         return edge_list
     
-    def get_adrecs_side_effect_hierarchical_edges(self, label="side_effect_is_a_side_effect"):
+    def get_adrecs_side_effect_hierarchical_edges(self, label: str = "side_effect_is_a_side_effect") -> list[tuple]:
         if not hasattr(self, "adrecs_ontology"):
             self.download_adrecs_data()
            
-        print("Started writing side effect hierarchical edges")
+        logger.info("Started writing side effect hierarchical edges")
         
         edge_list = []
         for relation in tqdm(self.adrecs_ontology):
@@ -299,7 +343,7 @@ class SideEffect:
                 
         return edge_list 
         
-    def add_prefix_to_id(self, prefix=None, identifier=None, sep=":") -> str:
+    def add_prefix_to_id(self, prefix: str = None, identifier: str = None, sep: str = ":") -> str:
         """
         Adds prefix to database id
         """
@@ -319,3 +363,21 @@ class SideEffect:
                 _list.append(e)
                 
         return joiner.join(list(dict.fromkeys(_list).keys()))
+    
+    def set_node_fields(self, side_effect_node_fields):
+        if side_effect_node_fields:
+            self.side_effect_node_fields = side_effect_node_fields
+        else:
+            self.side_effect_node_fields = [field.value for field in SideEffectNodeField]
+
+    def set_edge_fields(self, drug_side_effect_edge_fields):
+        if drug_side_effect_edge_fields:
+            self.drug_side_effect_edge_fields = drug_side_effect_edge_fields
+        else:
+            self.drug_side_effect_edge_fields = [field.value for field in DrugSideEffectEdgeField]
+
+    def set_edge_types(self, edge_types):
+        if edge_types:
+            self.edge_types = edge_types
+        else:
+            self.edge_types = [field for field in SideEffectEdgeType]
